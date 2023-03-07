c32250de7206fdb23cded0ef8b7a38edc10f248c
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2023-03-07 13:07:07 -0600

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-slate-500 '>
+      asd
+    </div>
+  )
+}
+
+export default App
