256d1874848747904c9b6124e42427f0541dfc4d
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2023-03-22 12:33:43 -0600

    pending: hostVans

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -11,11 +11,11 @@ function HostVans() {
   }, []);
 
   const hostVansElements = hostVans.map((van) => (
-    <article
-      key={van.id}
-      className="flex justify-between py-4 px-6 bg-white rounded-lg place-items-center"
-    >
-      <Link to={`/host/vans/${van.id}`}>
+    <Link to={`/host/vans/${van.id}`}  key={van.id}>
+      <article
+       
+        className="flex justify-between py-4 px-6 bg-white rounded-lg place-items-center"
+      >
         <div className="flex gap-4 place-items-center">
           <img className="w-16 rounded-lg" src={van.imageUrl} alt={van.name} />
           <div>
@@ -23,15 +23,17 @@ function HostVans() {
             <p className="text-custom-gray-100">${van.price}/day</p>
           </div>
         </div>
-      </Link>
-    </article>
+      </article>
+    </Link>
   ));
 
   return (
     <section className="font-inter bg-custom-orange-100 px-4 py-6">
       <h1 className="text-3xl font-bold">Your listed vans</h1>
       <div className="bg-custom-orange-100 py-6">
-        <div className="flex flex-col gap-4 mt-4">{hostVansElements}</div>
+        <div className="flex flex-col gap-4 mt-4">
+          {hostVans.length > 0 ? hostVansElements : <h2>Loading...</h2>}
+        </div>
       </div>
     </section>
   );
