9389dd0e53421215c63724315265af0f4df80b5c
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2023-03-27 19:50:45 -0600

    pending: search params

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,34 +1,42 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 function Vans() {
   const [vans, setVans] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
 
+  const filters = searchParams.get("type");
+  const filteredVans = filters
+    ? vans.filter((van) => van.type === filters)
+    : vans;
+  console.log(filteredVans)
   useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
       .then((data) => setVans(data.vans));
   }, []);
 
+  const capitalizeType = (type) => type.charAt(0).toUpperCase() + type.slice(1)
+
   const vanElements = vans.map((van) => (
     <div key={van.id}>
       <Link to={`/vans/${van.id}`}>
         <img className="rounded-md" src={van.imageUrl} alt={van.description} />
         <div className="flex justify-between font-semibold text-xl mt-4">
-            <p>{van.name}</p>
-            <span>${van.price}</span>
+          <p>{van.name}</p>
+          <span>${van.price}</span>
         </div>
         <p className="text-right text-sm">/day</p>
         <span
-            className={`py-2 capitalize px-4 ${
+          className={`py-2 capitalize px-4 ${
             van.type === "simple"
-                ? "bg-interactive-states-100"
-                : van.type === "luxury"
-                ? "bg-interactive-states-200"
-                : "bg-interactive-states-300"
-            } text-custom-orange-200 rounded-md font-semibold`}
+              ? "bg-interactive-states-100"
+              : van.type === "luxury"
+              ? "bg-interactive-states-200"
+              : "bg-interactive-states-300"
+          } text-custom-orange-200 rounded-md font-semibold`}
         >
-            {van.type}
+          { capitalizeType(van.type) }
         </span>
       </Link>
     </div>
@@ -39,12 +47,12 @@ function Vans() {
       <div>
         <h1 className="font-bold text-3xl">Explore our van options</h1>
         <div className="flex flex-wrap justify-between mt-4">
-          <span className="py-2 px-6 bg bg-custom-orange-200">Simple</span>
-          <span className="py-2 px-6 bg bg-custom-orange-200">Luxury</span>
-          <span className="py-2 px-6 bg bg-custom-orange-200">Rugged</span>
-          <span className="py-2 px-6 underline underline-offset-4">
+          <button onClick={() => setSearchParams({ type: 'simple'})} className="py-2 px-6 bg bg-custom-orange-200">Simple</button>
+          <button onClick={() => setSearchParams({ type: 'luxury'})} className="py-2 px-6 bg bg-custom-orange-200">Luxury</button>
+          <button onClick={() => setSearchParams({ type: 'rugged'})} className="py-2 px-6 bg bg-custom-orange-200">Rugged</button>
+          <button onClick={() => setSearchParams({})} className="py-2 px-6 underline underline-offset-4">
             Clear filters
-          </span>
+          </button>
         </div>
       </div>
       <div className="grid grid-cols-2 gap-6 mt-14">{vanElements}</div>
